39494f329f68d366ca335d7aa11affb6b160fe17
xavieur <one@email.com>
2020-12-01 10:46:37 +0100

    fix: order

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,7 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-var key = '9353d7f8d0ffc410966b55287511f6bb';
-var city = 'Barcelona';
-var url = "https://api.openweathermap.org/data/2.5/weather?q=".concat(city, "&appid=").concat(key);
-
-var getWeather = /*#__PURE__*/function () {
+var getCountry = /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
     var result;
     return regeneratorRuntime.wrap(function _callee$(_context) {
@@ -16,14 +12,13 @@ var getWeather = /*#__PURE__*/function () {
         switch (_context.prev = _context.next) {
           case 0:
             _context.next = 2;
-            return axios.get(url);
+            return axios.get('https://restcountries.eu/rest/v2/all');
 
           case 2:
             result = _context.sent;
-            console.log(result.data.main);
-            render(result.data.main.temp, result.data.main.humidity, result.data.main.pressure);
+            console.log('result: ', result);
 
-          case 5:
+          case 4:
           case "end":
             return _context.stop();
         }
@@ -31,66 +26,16 @@ var getWeather = /*#__PURE__*/function () {
     }, _callee);
   }));
 
-  return function getWeather() {
+  return function getCountry() {
     return _ref.apply(this, arguments);
   };
 }();
 
-getWeather();
-/* --source-maps --watch --presets=@babel/preset-env,@babel/preset-react 
-
-"dependencies": {
-    "@babel/polyfill": "^7.12.1",
-    "core-js": "^3.8.0"
-  }*/
-
-/* 
-const personA = {
-    name: 'Perico',
-    age: 26,
-    city: 'Reus'
-}
-
-const sumar = ()=>{
-    personA.age = personA.age + 1
-    // personA.age += 1
-    // personA.age ++
-    render()
-}
-const restar = ()=>{
-    personA.age = personA.age - 1
-    // personA.age -= 1
-    // personA.age -- 
-    render()
-}
-const reset = ()=>{
-    personA.age = 0
-    render()
-} */
-
-/* const appRoot = document.getElementById('appRoot')
-
-const render = ()=>{
-    const template = (
-        <div>
-            <h1>Nombre: {personA.name}</h1>
-            <p>Edad: {personA.age}</p>
-            <p>Ciudad: {personA.city}</p>
-            <button onClick={sumar}>+1</button>
-            <button onClick={restar}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    )
-    
-    ReactDOM.render(template, appRoot)
-}
-
-render() */
-
+getCountry();
 var appRoot = document.getElementById('appRoot');
 
-var render = function render(temperatura, humedad, presion) {
-  var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Tiempo en ", city, ":"), /*#__PURE__*/React.createElement("p", null, "Temperatura: ", temperatura), /*#__PURE__*/React.createElement("p", null, "Humedad: ", humedad), /*#__PURE__*/React.createElement("p", null, "Presi\xF3n: ", presion));
+var render = function render(country) {
+  var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, country));
   ReactDOM.render(template, appRoot);
 };
 
---
+++ src/app-weather.js
@@ -0,0 +1,42 @@
+const key = '9353d7f8d0ffc410966b55287511f6bb'
+const city = 'Barcelona'
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+
+
+const getWeather = async ()=>{
+    const result = await axios.get(url)
+    render(result.data.main.temp, result.data.main.humidity, result.data.main.pressure, result.data.weather[0].description)
+}
+
+getWeather()
+
+
+const appRoot = document.getElementById('appRoot')
+
+const render = (temperatura, humedad, presion, descripcion)=>{
+    const template = (
+        <div>
+            <h1>Tiempo en {city}:</h1>
+            <p>En pocas palabras: {descripcion}</p>
+            <p>Temperatura: {temperatura}</p>
+            <p>Humedad: {humedad}</p>
+            <p>Presión: {presion}</p>
+        </div>
+    )
+    
+    ReactDOM.render(template, appRoot)
+}
+
+render()
+
+
+
+
+/* --source-maps --watch --presets=@babel/preset-env,@babel/preset-react 
+
+"dependencies": {
+    "@babel/polyfill": "^7.12.1",
+    "core-js": "^3.8.0"
+  }*/
+
